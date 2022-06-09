bf6af3a1d8f559992ff9c0c84d5b21cbd7c85e09
rjoydip <joydipand@gmail.com>
2022-06-09 17:49:20 +0530

    fix: playground styles issue fixing

--- src/pages/playground/index.tsx
+++ src/pages/playground/index.tsx
@@ -1,56 +1,49 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
+import {
+  LiveProvider,
+  LiveEditor,
+  LiveError,
+  LivePreview
+} from 'react-live'
 import Layout from '@theme/Layout';
-import CodeMirror from '@uiw/react-codemirror';
-import { javascript } from '@codemirror/lang-javascript';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import { Logs } from '@site/src/components/global/Logs';
 
 import "./playground.module.scss"
 
+import { Logs } from '@site/src/components/global/Logs';
+import { EditorTheme } from './theme';
+
 export default function Home(): JSX.Element {
-  const previewRef = useRef(null);
-  const [code, setCode] = useState("console.log('hello world!');");
   const { siteConfig } = useDocusaurusContext();
 
   return (
     <Layout
       title={siteConfig.title}
       description={siteConfig.tagline}>
-      <div className="playground">
-        <div className='row'>
-          <div className="col col--7" style={{
-            padding: "0",
-          }}>
-            <div className='editor'>
-              <CodeMirror
-                value={code}
-                height="100%"
-                theme={"dark"}
-                onKeyDown={(event) => {
-                  if ((event.ctrlKey || event.metaKey) && String.fromCharCode(event.which).toLowerCase() === 's') {
-                    event.preventDefault();
-                    new Function(code)();
-                  } else {
-                    return event;
-                  }
-                }}
-                extensions={[javascript({ jsx: true })]}
-                onChange={(value, _) => setCode(value)}
-              />
-            </div>
-          </div>
-          <div className="col col--5" style={{
-            padding: "0",
-          }}>
-            <div style={{
-              height: "100%",
+      <LiveProvider code="<strong>Hello World!</strong>">
+        <div className="playground">
+          <div className='row'>
+            <div className="col col--7" style={{
+              padding: "0",
             }}>
-              <div id="preview" className='preview' ref={previewRef}></div>
-              <div className='logger'>{<Logs />}</div>
+              <div className='editor'>
+                <LiveEditor language="jsx" theme={EditorTheme}/>
+              </div>
+            </div>
+            <div className="col col--5" style={{
+              padding: "0",
+            }}>
+              <div style={{
+                height: "100%",
+              }}>
+                <div className='preview'><LivePreview />
+                  <LiveError /></div>
+                <div className='logger'><Logs /></div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </LiveProvider>
     </Layout>
   );
 }
---
+++ src/pages/playground/theme.ts
@@ -0,0 +1,95 @@
+import { PrismTheme } from "prism-react-renderer";
+
+export const EditorTheme: PrismTheme = {
+  styles: [
+    {
+      types: ['prolog', 'comment', 'doctype', 'cdata'],
+      style: {
+        color: '6272a4'
+      }
+    },
+    {
+      types: ['property', 'tag', 'constant', 'symbol', 'deleted'],
+      style: { color: '#ff79c6' }
+    },
+    {
+      types: ['boolean', 'number'],
+      style: { color: '#bd93f9' }
+    },
+    {
+      types: ['selector', 'attr-name', 'string', 'char', 'builtin', 'insterted'],
+      style: {
+        color: '#50fa7b'
+      }
+    },
+    {
+      types: [
+        'operator',
+        'entity',
+        'url',
+        'string',
+        'style',
+        'variable',
+        'language-css'
+      ],
+      style: {
+        color: '#f8f8f2',
+        cursor: 'help'
+      }
+    },
+    {
+      types: ['deleted'],
+      style: {
+        color: 'rgb(255, 85, 85)'
+      }
+    },
+    {
+      types: ['italic'],
+      style: {
+        fontStyle: 'italic'
+      }
+    },
+    {
+      types: ['important', 'bold'],
+      style: {
+        fontWeight: 'bold'
+      }
+    },
+    {
+      types: ['regex', 'important'],
+      style: {
+        color: '#ffb86c'
+      }
+    },
+    {
+      types: ['atrule', 'attr-value', 'function', 'class-name'],
+      style: {
+        color: '#f1fa8c'
+      }
+    }, {
+      types: ['keyword'],
+      style: {
+        color: '#8be9fd'
+      }
+    }, {
+      types: ['punctuation'],
+      style: {
+        color: '#f8f8f2'
+      }
+    },
+    {
+      types: ['punctuation', 'symbol', 'namespace'],
+      style: {
+        opacity: 0.7
+      }
+    }
+  ],
+  plain: {
+    color: "#f8f8f2",
+    backgroundColor: "#282a36",
+    fontStyle: "italic",
+    fontWeight: "normal",
+    textDecorationLine: "none",
+    opacity: 1
+  }
+};
