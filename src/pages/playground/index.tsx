f4f1cac00a89af554550043a32f26545c4dd3adb
rjoydip <joydipand@gmail.com>
2022-06-13 23:16:15 +0530

    feat(playground): frame title bar with icon added

--- src/pages/playground/index.tsx
+++ src/pages/playground/index.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
-import {
-  LiveProvider,
-  LiveEditor,
-  LiveError,
-  LivePreview
-} from 'react-live'
-import Layout from '@theme/Layout';
-import SplitPane from 'react-split-pane';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import React from "react";
+import { LiveEditor, LiveError, LivePreview, LiveProvider } from "react-live";
+import Layout from "@theme/Layout";
+import SplitPane from "react-split-pane";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
-import "./playground.module.scss"
+import "./playground.module.scss";
 
-import { ConsoleL } from '@site/src/components/global/ConsoleL';
-import { EditorTheme } from './theme';
+import { ConsoleL } from "@site/src/components/global/ConsoleL";
+import { EditorTheme } from "./theme";
+
+function Window(props): JSX.Element {
+  return (
+    <div className="ch-frame-container">
+      <div className="ch-frame-title-bar">
+        <div className="ch-frame-buttons">
+          <div className="ch-frame-button ch-frame-button-left"></div>
+          <div className="ch-frame-button-space"></div>
+          <div className="ch-frame-button ch-frame-button-middle"></div>
+          <div className="ch-frame-button-space"></div>
+          <div className="ch-frame-button ch-frame-button-right"></div>
+        </div>
+      </div>
+      {props.children}
+    </div>
+  );
+}
 
 export default function PlaygroundPage(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
@@ -20,17 +32,28 @@ export default function PlaygroundPage(): JSX.Element {
   return (
     <Layout
       title={siteConfig.title}
-      description={siteConfig.tagline}>
+      description={siteConfig.tagline}
+    >
       <LiveProvider code="<strong>Hello World!</strong>">
         <div className="playground">
           <SplitPane split="vertical" defaultSize="50%" minSize="400px">
-            <div className='editor'>
-              <LiveEditor language="jsx" theme={EditorTheme} />
-            </div>
+            <Window>
+              <div className="editor">
+                <LiveEditor language="jsx" theme={EditorTheme} />
+              </div>
+            </Window>
             <SplitPane split="horizontal" defaultSize="60%">
-              <div className='preview'><LivePreview />
-                <LiveError /></div>
-              <div className='console'><ConsoleL /></div>
+              <Window>
+                <div className="preview">
+                  <LivePreview />
+                  <LiveError />
+                </div>
+              </Window>
+              <div className="console">
+                <Window>
+                  <ConsoleL />
+                </Window>
+              </div>
             </SplitPane>
           </SplitPane>
         </div>
